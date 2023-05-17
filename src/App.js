7e2486b90a169a7d7678cce43a732ab492e0cf51
Nirav9603 <niravmagatarapara111@gmail.com>
2023-05-17 14:23:52 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import { useState } from 'react';
+import AddModal from './modal/Modal';
+import EditModal from './modal/EditModal';
 
 function App() {
+
+  const [show, setShow] = useState(false);
+  const [eshow, seteShow] = useState(false);
+
+
+  let localData = JSON.parse(localStorage.getItem("Data"))
+
+  const handleAdd = () => {
+    setShow(true)
+  }
+
+  const handleDelete = (id) => {
+    let Ddata = localData.filter((l) => l.id !== id)
+    localStorage.setItem("Data", JSON.stringify(Ddata))
+    window.location.reload();
+  }
+
+  const handleEdit = (data) => {
+    seteShow(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddModal show={show} setShow={setShow} />
+      <EditModal show={eshow} setShow={seteShow} />
+      <div className="App">
+        <h1>TO DO LIST</h1>
+        <Button variant="primary" onClick={() => handleAdd()}>Add Items</Button>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Item 1</th>
+              <th>Item 2</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              localData.map((d,i) => {
+                return (
+                  <tr key={i}>
+                    <td>{d.title}</td>
+                    <td>{d.item1}</td>
+                    <td>{d.item2}</td>
+                    <td><Button onClick={() => handleEdit(d)} variant="primary">Edit</Button> <Button onClick={() => handleDelete(d.id)} variant="danger">Delete</Button></td>
+                  </tr>
+                )
+              })  
+            }
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
 
---
+++ src/modal/EditModal.js
@@ -0,0 +1,77 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function EditModal({ show, setShow }) {
+
+    const handleClose = () => setShow(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const formData = {
+            id: Math.random(),
+            title: event.target.title.value,
+            item1: event.target.item1.value,
+            item2: event.target.item2.value
+        }
+
+        let localData = JSON.parse(localStorage.getItem("Data"))
+
+        if(localData === null){
+            localStorage.setItem("Data", JSON.stringify([formData]))
+        }else{ 
+            localData.push(formData)
+            localStorage.setItem("Data", JSON.stringify(localData))
+        }
+        handleClose()
+    }
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Buy Milk"
+                                name="title"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Item 1</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Store"
+                                name="item1"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Item 2</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Mart"
+                                name="item2"
+                            />
+                        </Form.Group>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button type='submit' variant="primary">
+                                Add
+                            </Button>
+                        </Modal.Footer>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+export default EditModal;
